be148bde7d1e5885810c000be48671aedb3ec751
VaibhavMojidra <vmojidra25@gmail.com>
2021-07-25 00:31:17 +0530

    Dagger2ConstructorInjectionJava

---
+++ app/src/main/java/com/vaibhavmojidra/dagger2constructorinjectionjava/SmartPhoneComponent.java
@@ -0,0 +1,8 @@
+package com.vaibhavmojidra.dagger2constructorinjectionjava;
+
+import dagger.Component;
+
+@Component
+public interface SmartPhoneComponent {
+    public SmartPhone getSmartPhone();
+}
